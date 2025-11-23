a5e731908df8efc857d61661d49b88bda98a98f4
Samiul Islam Ramim <123535324+SamiulIslamRamim@users.noreply.github.com>
2025-11-23 11:40:25 +0600

    day-2 office server set

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Test
 
 <!-- https://medium.com/@gabrieldrouin/node-js-2025-guide-how-to-setup-express-js-with-typescript-eslint-and-prettier-b342cd21c30d -->
+
+
+installed needed dependencies and all the other things for a simple api. -> 1
+will understand the fundamentals of eslint -> ....
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-// index.js
+// index.ts
 import { middleware } from "#middlewares/middlewares.js";
 import express from "express";
-const app = express();
-const port = "3000";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  console.log("Response sent");
-});
+const app = express();
+const port = process.env.PORT ?? "9001";
+
+app.get("/", middleware);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
